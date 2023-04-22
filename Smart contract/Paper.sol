7627622d64d371e76878ce996a80dcd79ca15b9c
Shivanshu Suryakar <jiminnpark007@gmail.com>
2023-04-22 11:00:10 +0530

    Updated the smart contract : added new event for plagarised content identification

--- Smart contract/Paper.sol
+++ Smart contract/Paper.sol
@@ -6,47 +6,37 @@ import "hardhat/console.sol";
 contract Research{
     // uint public paperId;
     uint public totalChunks;
-    // address payable public publisherAddr;
-
-    // struct Paper{
-    //     uint Id;
-    //     string content;
-    //     address owner;
-    // }
-
-    // mapping(address => Paper[]) public publisher;
+    
+    struct Ans{
+        bool isValid;
+        uint plagiarism;
+    }
 
     mapping(string => uint) public chunks;
 
-    // constructor(string[] memory chunkArr){
-    //     for(uint i = 0; i < chunkArr.length; i++){
-    //         totalChunks++;
-    //         chunks[chunkArr[i]] = 1;
-    //         console.log("%d is val", chunks[chunkArr[i]]);
-    //     }
-    // }
-
-
     // events
-    event validation(address indexed sender, bool isValid,uint plag_percent);
+    event validation(address indexed sender, bool isValid, uint percentPlag);
+    event plag_detect(uint[] plag_part);
 
 // takes input as array of string and returns bool
     function validate(string[] memory _contentHash) public returns(bool){
         string[] memory arr = _contentHash;
-        uint percentPlag = plagarism(_contentHash);
+        uint n = _contentHash.length;
 
+        uint percentPlag = plagarism(_contentHash, n);
+        
         if(percentPlag < 20){
-            uint n = arr.length;
-            for(uint i = 0; i < n; i++){
+            uint n1 = arr.length;
+            for(uint i = 0; i < n1; i++){
                 if(chunks[arr[i]] == 0){
                     update(_contentHash[i], 1);
                     totalChunks++;
                 }
             }
-            emit validation(msg.sender, true,percentPlag);
+            emit validation(msg.sender, true, percentPlag);
             return true;
         }
-        emit validation(msg.sender, false,percentPlag);
+        emit validation(msg.sender, false, percentPlag);
         return false;
     }
 
@@ -56,15 +46,18 @@ contract Research{
     }
 
 // checks plagiarism 
-    function plagarism(string[] memory _contentHash) public view returns(uint){
-        uint count = 0;
-        uint n = _contentHash.length;
+    function plagarism(string[] memory _contentHash, uint n) public returns(uint percent){
+        uint count = 0;  
+        uint[] memory plag_part = new uint[](n);
+        uint ind=0;
         for(uint i = 0; i < n; i++){
             if(chunks[_contentHash[i]] == 1){
                 count++;
+                plag_part[ind++] = i+1;
             }
         }
 
+        emit plag_detect(plag_part);
         return (count*100/n);
     }
 }
